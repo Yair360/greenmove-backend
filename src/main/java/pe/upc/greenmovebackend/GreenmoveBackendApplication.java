a97ba0dcec8bce66c4c569bab127830bbe7b079b
Yair Aru Acevedo <u202125984@upc.edu.pe>
2024-10-17 19:16:40 -0500

    chore: initial commit.

---
+++ src/main/java/pe/upc/greenmovebackend/GreenmoveBackendApplication.java
@@ -0,0 +1,13 @@
+package pe.upc.greenmovebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenmoveBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenmoveBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/upc/greenmovebackend/GreenmoveBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.upc.greenmovebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenmoveBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
